72243bed9447178c894ac6bb2f70fcc71eff5124
Eya MAllouki <100727922+eyamallouki@users.noreply.github.com>
2023-04-30 22:50:12 +0100

    ProjectWithHomePage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HomePageComponent } from './Shared/home-page/home-page.component';
+import { NavBarComponent } from './Shared/nav-bar/nav-bar.component';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
