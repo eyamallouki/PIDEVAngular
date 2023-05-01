479c0181ee018d77f6679b4670735ae4dae66310
AzzaBenRomdhan <azza.benromdhan@esprit.tn>
2023-05-01 01:31:58 +0100

    Template updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HomePageComponent } from './Shared/home-page/home-page.component';
 import { NavBarComponent } from './Shared/nav-bar/nav-bar.component';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FooterComponent } from './Shared/footer/footer.component';
+import { HomeComponent } from './Shared/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent,
-    NavBarComponent
+    NavBarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
